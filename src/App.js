050afc3564b5486ea0a94869f5aecdd0fdaa2704
andre <andre_w3@yahoo.com>
2021-11-29 20:43:52 -0500

    installed fonts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import img from '../src/image/nyc.JPG';
 import LandingPageCircle from './components/LandingPageCircle/LandingPageCircle';
 import LandingPageContainer from './components/LandigPageContainer/LandingPageContainer';
 import LandingPageModal from './components/LandingPageModal/LandingPageModal';
-// import LandingPageFooter from './components/LandingPageFooter/LandingPageFooter';
+import LandingPageFooter from './components/LandingPageFooter/LandingPageFooter';
 // import LandingPageModal
 
 import './App.css';
@@ -17,13 +17,12 @@ function App() {
       }}>
       <div className="filter-shade">
         <div className="small-container">
+        <LandingPageCircle />
           <LandingPageContainer />
-          <LandingPageCircle />
           <LandingPageModal />
           <div className="contact-icons" alt="social media icons">
-          
+          <LandingPageFooter />
           </div>
-        
         </div>
       </div>
     </div>
