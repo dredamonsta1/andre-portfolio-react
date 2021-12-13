ea07141f93b1c6e179578c7d9ada0d471ffe2eca
andre <andre_w3@yahoo.com>
2021-12-13 03:26:46 -0500

    added contact form

--- src/components/ModalContact/ModalContact.js
+++ src/components/ModalContact/ModalContact.js
@@ -13,6 +13,25 @@ const ModalContact = () => {
             <Modal show={modal} close={toggle} title="Contact">
         
             This is the Contact modal more soon 
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                
+                </label>
+                <label>
+                    Email:
+                    <input type="email" email="email"/>
+                </label>
+
+                <label>
+                    Message:
+                    <input type="text" message="message" />
+                </label>
+                <input type="submit" value="submit" />
+                <input type="reset" value="reset" />
+            
+            </form>
             </Modal>
         </>
     )
