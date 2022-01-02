f2690715287c3110321821f91597de294bd6760e
andre <andre_w3@yahoo.com>
2022-01-02 02:24:45 -0500

    working on express server

---
+++ server.js
@@ -0,0 +1,20 @@
+// const url = `${getInfuraUrl(message.chainId)}/8fcacee838e04f31b6ec145eb98879c8`
+
+// console.log(url)
+
+
+// const getInfuraUrl = (chainId: string) => {
+//     switch (Number.parseInt(chainId)) {
+//         case 1:
+//             return 'https://mainnet.infura.io/v3';
+//         case 3:
+//             return 'https://ropsten.infura.io/v3';
+//         case 4:
+//             return 'https://rinkeby.infura.io/v3';
+//         case 5:
+//             return 'https://goerli.infura.io/v3';
+//         case 137:
+//             return 'https://polygon-mainnet.infura.io/v3';
+//     }
+// };
+
