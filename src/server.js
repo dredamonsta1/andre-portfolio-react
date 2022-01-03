71ae11aa6c02ef0a84a65a750b017d3ce283c603
andre <andre_w3@yahoo.com>
2022-01-03 01:40:23 -0500

    working on express server

--- src/server.js
+++ src/server.js
@@ -1,20 +1,3 @@
-// const url = `${getInfuraUrl(message.chainId)}/8fcacee838e04f31b6ec145eb98879c8`
+const PORT = 4361;
 
-// console.log(url)
-
-
-// const getInfuraUrl = (chainId: string) => {
-//     switch (Number.parseInt(chainId)) {
-//         case 1:
-//             return 'https://mainnet.infura.io/v3';
-//         case 3:
-//             return 'https://ropsten.infura.io/v3';
-//         case 4:
-//             return 'https://rinkeby.infura.io/v3';
-//         case 5:
-//             return 'https://goerli.infura.io/v3';
-//         case 137:
-//             return 'https://polygon-mainnet.infura.io/v3';
-//     }
-// };
-
+import Express from 'express';
