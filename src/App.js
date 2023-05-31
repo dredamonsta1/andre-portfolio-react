2d6e3cff800f9a90f79bfb96d47d00e121110c87
andre wilkinson <andrefullstack@andres-MBP-2.fios-router.home>
2023-05-31 01:24:00 -0400

    change hook to use-local-storage for dark mode

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import { createContext, useState } from 'react';
+import useLocalStorage from 'use-local-storage';
 // import img from '../src/image/nyc.JPG';
 import LandingPageCircle from './components/LandingPageCircle/LandingPageCircle';
 import LandingPageContainer from './components/LandigPageContainer/LandingPageContainer';
 import LandingPageModal from './components/LandingPageModal/LandingPageModal';
 import LandingPageFooter from './components/LandingPageFooter/LandingPageFooter';
-import AppButton from './components/button/AppButton';
+// import AppButton from './components/button/AppButton';
 import ReactSwitch from 'react-switch';
 // import LandingPageModal
 
 import './App.css';
 
-export const themeContext = createContext("null");
+// export const themeContext = createContext("null");
 
 function App() {
-  const [theme, setTheme] = useState("dark")
+  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+
+    
+    // setTheme((curr) => (curr === "light" ? "dark" : "light"));
   }
 
   return (
-    <themeContext.Provider value={{ theme, toggleTheme }}>
-      {/* <div className='app' id={theme}> */}
+    // <themeContext.Provider value={{ theme, toggleTheme }}>
+      <div className='app' data-theme={theme}>
         <div className="app-background" id={theme}>
               <div className='switch'>
                 <label>{theme === "light" ? "🌝" : "🌑"}</label>
                 <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
               </div>
           {/* <div className="filter-shade"> */}
-              <AppButton />
+              {/* <AppButton /> */}
             <div className="small-container">
               <LandingPageCircle />
               <LandingPageContainer />
@@ -39,8 +44,8 @@ function App() {
             </div>
           {/* </div> */}
         </div>
-      {/* </div> */}
-    </themeContext.Provider>
+      </div>
+    // </themeContext.Provider>
   );
 }
 export default App;
