6d9e1ff2107cb03f08f2c3c8cc6c4f54e91fb6e5
Andre Wilkinson <andre_w3@yahoo.com>
2023-08-12 23:21:30 -0400

    medium logo

--- src/components/LandingPageFooter/LandingPageFooter.js
+++ src/components/LandingPageFooter/LandingPageFooter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXTwitter, faYoutube, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter, faYoutube, faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
 import styles from "./LandingPageFooter.module.css";
 
 
@@ -10,12 +10,14 @@ function LandingPageFooter() {
     const dreGit = <FontAwesomeIcon className='dreGit' icon={faGithub}  hover="red"size="2x" color="yellow" />
     const dreYoutube = <FontAwesomeIcon icon={faYoutube} size="2x" color="red"/>
     const dreLinkedin = <FontAwesomeIcon icon={faLinkedin} size="2x" color="gray"/>
+    const dreMedium = <FontAwesomeIcon className='dreMed'icon={faMedium} size='2x'/>
     return (
         <div className={styles.contactIcons}>           
             <a className='linkedIn' href="https://www.linkedin.com/in/andre-wilkinson/" target="blank">{dreLinkedin}</a>
             <a className='youtube' href="https://www.youtube.com/watch?v=6Us5s49NBwY" target="blank">{dreYoutube}</a>
             <a className='github' href="https://github.com/dredamonsta1" target="blank">{dreGit}</a>
             <a className='twitter' href="https://twitter.com/dredamonsta1" target="blank">{dreTweet}</a>
+            <a className='medium' href="https://medium.com/@andrefullstack" target="blank">{dreMedium}</a>
         </div>
     ); 
 } 
