5a33a1dacc8f65c2ff0efe798f2fc589a1348119
Andre Wilkinson <andre_w3@yahoo.com>
2023-10-20 03:00:53 -0400

    cleaning up code

--- src/components/Pdf/PdfDocument.js
+++ src/components/Pdf/PdfDocument.js
@@ -2,34 +2,13 @@ import React from "react";
 // import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 
-// const styles = StyleSheet.create({
-//     page: {
-//       flexDirection: 'row',
-//       backgroundColor: '#E4E4E4'
-//     },
-//     section: {
-//       margin: 10,
-//       padding: 10,
-//       flexGrow: 1
-//     }
-//   });
 
-  // Create Document Component
+  // Create Document Component modal
 const PdfDocument = ({onClose}) => (
-  <>
+  <div className="modal">
     <h1>Hello</h1>
     <button onClick={onClose}>Close</button>
-  </>
-    // <Document>
-    //   <Page size="A4" style={styles.page}>
-    //     <View style={styles.section}>
-    //       <Text>Section #1</Text>
-    //     </View>
-    //     <View style={styles.section}>
-    //       <Text>Section #2</Text>
-    //     </View>
-    //   </Page>
-    // </Document>
+  </div>
   );
 
   export default PdfDocument;
