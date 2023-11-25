930a270bd12dda5cf1c5c96abdd323c371c246b8
Andre Wilkinson <andre_w3@yahoo.com>
2023-11-25 15:13:05 -0500

    dired up pdf button click

--- src/components/Pdf/Pdf.js
+++ src/components/Pdf/Pdf.js
@@ -2,22 +2,32 @@ import { React, useState} from 'react';
 import { createPortal } from "react-dom";
 // import PDF from "react-pdf-js";
 import PdfDocument from './PdfDocument.js';
+// import { Document, Page } from 'react-pdf';
+// import andrefullstackR from '../../image'
 
+// const PDFModal = ({ }) => {
 
-
-function Pdf() {
+// }
+const Pdf = () => {
     const [showModal, setShowModal] = useState(false);
-    // const onClick = () => {
-    //     console.log('clicked me')
-    // }
+    const onClick = () => {
+        setShowModal(true)
+        console.log('clicked me')
+    }
 
    
 
     return (
         <>
-            <button aria-label="pdf button" onClick={() => setShowModal(true)}>
+            <button aria-label="pdf button" onClick={onClick}>
                 My PDF
             </button>
+
+            {/* <div style={{ width: '100%', height: '500px' }}>
+          <Document file={andrefullstackR}>
+            <Page pageNumber={1} />
+          </Document>
+        </div> */}
 
             {showModal && createPortal(
         <PdfDocument onClose={() => setShowModal(false)} />,
